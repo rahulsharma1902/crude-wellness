d7fbc4dfaf36fdf22036cc246a5e607a5f33d7c1
rahulsharma1902 <rahulanku1902@gmail.com>
2023-10-16 11:44:48 +0530

    new

--- app/Http/Controllers/Front/FrontShopController.php
+++ app/Http/Controllers/Front/FrontShopController.php
@@ -3,11 +3,19 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Categories;
+use App\Models\Media;
+use App\Models\Products;
+use App\Models\ProductVariations;
+use Illuminate\Support\Facades\DB;
 
 class FrontShopController extends Controller
 {
     public function index(){
-
+        $categories = Categories::whereNull('parent_category')->with('products')->get()->toArray();
+        echo '<pre>';
+        print_r($categories);
+        die();
         return view('front.shop.index');
     }
     public function shopdetail(){
--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -12,4 +12,7 @@ class Categories extends Model
     public function parentCategory(){
         return $this->hasOne(Categories::class,'id','parent_category');
     }
+    public function products(){
+        return $this->hasMany(Products::class,'category_id','id');
+    }
 }
